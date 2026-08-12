15f78baf24e3afd27c993cc3524cb87f7a8383d0
TunHigh <87038107+TunHigh@users.noreply.github.com>
2026-08-13 02:46:23 +0700

    Update minesweeper.js

--- src/commands/Fun/minesweeper.js
+++ src/commands/Fun/minesweeper.js
@@ -1,8 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
 import { Minesweeper } from 'discord-gamecord';
 import { logger } from '../../utils/logger.js';
-import { InteractionHelper } from '../../utils/interactionHelper.js';
-import { errorEmbed } from '../../utils/embeds.js';
 import { replyUserError, ErrorTypes } from '../../utils/errorHandler.js';
 
 export default {
@@ -13,14 +11,11 @@ export default {
 
   async execute(interaction, config, client) {
     try {
-      // Defer the interaction first
-      await InteractionHelper.safeDefer(interaction);
-
       const Game = new Minesweeper({
         message: interaction,
         embed: {
           title: '💣 Minesweeper',
-          color: 3066993, // Blue color code
+          color: '#2ECC71',
           timestamp: true,
           description: 'Click vào nút để lật các ô ngoại trừ các ô mìn. Hãy cẩn thận!',
         },
@@ -32,11 +27,11 @@ export default {
         timeoutTime: 60000,
         winMessage: 'Bạn đã chiến thắng! Bạn đã tránh được tất cả các mìn. 🎉',
         loseMessage: 'Bạn đã thua! Hãy cẩn thận hơn với các mìn lần sau. 💥',
-        othersMessage: 'Chỉ <@{{author}}> mới có thể sử dụng các nút lệnh!',
+        playerOnlyMessage: 'Chỉ <@{{author}}> mới có thể sử dụng các nút lệnh!',
         isSlashGame: true,
       });
 
-      Game.startGame();
+      await Game.startGame();
       logger.debug(`Minesweeper command executed by user ${interaction.user.id} in guild ${interaction.guildId}`);
     } catch (error) {
       logger.error('Minesweeper command error:', error);
