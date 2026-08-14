13f3c88fde799b81efe0a1d415baccedc5871aab
TunHigh <87038107+TunHigh@users.noreply.github.com>
2026-08-15 01:47:47 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ import { getServerCounters, saveServerCounters, updateCounter } from './services
 import { logger, startupLog, shutdownLog } from './utils/logger.js';
 import { checkBirthdays } from './services/birthdayService.js';
 import { checkGiveaways } from './services/giveawayService.js';
-import { checkCommunityMinesweeperGames } from './services/communityMinesweeperService.js';
 import { loadCommands, registerCommands as registerSlashCommands } from './handlers/loaders/commandLoader.js';
 import { runSafeTask, handleTaskError, ErrorCodes } from './utils/errorHandler.js';
 import { initializeMusic } from './services/music/riffySetup.js';
@@ -251,7 +250,6 @@ class TitanBot extends Client {
   setupCronJobs() {
     cron.schedule('0 6 * * *', runSafeTask('birthday_check', () => checkBirthdays(this)));
     cron.schedule('* * * * *', runSafeTask('giveaway_check', () => checkGiveaways(this)));
-    cron.schedule('* * * * *', runSafeTask('community_minesweeper_check', () => checkCommunityMinesweeperGames(this)));
     cron.schedule('*/15 * * * *', runSafeTask('counter_update', () => this.updateAllCounters()));
   }
 
