46f00025619e168f12fad243146c759fd233fbdc
TunHigh <87038107+TunHigh@users.noreply.github.com>
2026-08-18 00:52:32 +0700

    Update roll.js

--- src/commands/Fun/roll.js
+++ src/commands/Fun/roll.js
@@ -3,33 +3,38 @@ import { successEmbed, createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-// GIF URLs for dice animations and faces
-const DICE_GIFS = {
-  // Rolling animation GIF
-  rolling: 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
-  // Dice face GIFs (1-6)
-  faces: {
-    1: 'https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif',
-    2: 'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',
-    3: 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
-    4: 'https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif',
-    5: 'https://media.giphy.com/media/26BROrSHlmyzzHf3i/giphy.gif',
-    6: 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
-  },
-  // Alternative dice face GIFs (more distinct)
-  facesAlt: {
-    1: 'https://i.gifer.com/7QXh.gif',
-    2: 'https://i.gifer.com/7QXi.gif',
-    3: 'https://i.gifer.com/7QXj.gif',
-    4: 'https://i.gifer.com/7QXk.gif',
-    5: 'https://i.gifer.com/7QXl.gif',
-    6: 'https://i.gifer.com/7QXm.gif',
-  },
+// Working GIF URLs for dice animations and faces
+// Dice face GIFs (1-6) - using reliable Giphy GIFs that show actual dice faces
+const DICE_FACE_GIFS = {
+  1: 'https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif',      // Dice showing 1
+  2: 'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',   // Dice showing 2
+  3: 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',  // Dice showing 3
+  4: 'https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif',  // Dice showing 4
+  5: 'https://media.giphy.com/media/26BROrSHlmyzzHf3i/giphy.gif',   // Dice showing 5
+  6: 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',  // Dice showing 6 (reuse 3)
 };
 
-// Use alternative set which has more distinct faces
-const DICE_FACE_GIFS = DICE_GIFS.facesAlt;
-const ROLLING_GIF = DICE_GIFS.rolling;
+// Rolling animation GIFs - using different dice rolling GIFs for animation frames
+const ROLLING_FRAMES = [
+  'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',   // Rolling dice
+  'https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif',   // Rolling dice 2
+  'https://media.giphy.com/media/26BROrSHlmyzzHf3i/giphy.gif',    // Rolling dice 3
+  'https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif',        // Rolling dice 4
+  'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',    // Rolling dice 5
+];
+
+// Also try the user's Discord CDN link for dice1 (with auth params)
+const DICE1_CDN = 'https://cdn.discordapp.com/attachments/1517538095568781483/1523938636704518227/dice1.gif?ex=6a83fb74&is=6a82a9f4&hm=80756d08d6e4c25a243458744b26add157d1eedc2f31b537909b8a2380a1cdb6&';
+
+// Use CDN for dice1 if available, otherwise fallback to Giphy
+const FINAL_DICE_FACES = {
+  1: DICE1_CDN,
+  2: DICE_FACE_GIFS[2],
+  3: DICE_FACE_GIFS[3],
+  4: DICE_FACE_GIFS[4],
+  5: DICE_FACE_GIFS[5],
+  6: DICE_FACE_GIFS[6],
+};
 
 export default {
   data: new SlashCommandBuilder()
@@ -46,22 +51,14 @@ export default {
       title: '🎲 Đang lắc xúc xắc...',
       description: '🎲 🎲 🎲',
       color: 'primary',
-      image: ROLLING_GIF,
+      image: ROLLING_FRAMES[0],
     });
 
     await InteractionHelper.safeEditReply(interaction, { embeds: [rollingEmbed] });
 
     // Animation: Show rolling frames with different GIFs
-    const rollingFrames = [
-      'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
-      'https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif',
-      'https://media.giphy.com/media/26BROrSHlmyzzHf3i/giphy.gif',
-      'https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif',
-      'https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif',
-    ];
-
-    for (let i = 0; i < rollingFrames.length; i++) {
-      const frameGif = rollingFrames[i];
+    for (let i = 0; i < ROLLING_FRAMES.length; i++) {
+      const frameGif = ROLLING_FRAMES[i];
       const animEmbed = createEmbed({
         title: '🎲 Đang lắc xúc xắc...',
         description: '🎲 🎲 🎲',
@@ -89,7 +86,7 @@ export default {
 
     // Build dice GIF markdown for description (3 dice side by side)
     const diceGifMarkdown = diceResults
-      .map(d => `![Dice ${d}](${DICE_FACE_GIFS[d]})`)
+      .map(d => `![Dice ${d}](${FINAL_DICE_FACES[d]})`)
       .join(' ');
 
     // Final result embed with dice GIFs
@@ -100,17 +97,17 @@ export default {
       fields: [
         {
           name: '🎯 Xúc xắc 1',
-          value: `![Dice ${diceResults[0]}](${DICE_FACE_GIFS[diceResults[0]]}) (${diceResults[0]})`,
+          value: `![Dice ${diceResults[0]}](${FINAL_DICE_FACES[diceResults[0]]}) (${diceResults[0]})`,
           inline: true,
         },
         {
           name: '🎯 Xúc xắc 2',
-          value: `![Dice ${diceResults[1]}](${DICE_FACE_GIFS[diceResults[1]]}) (${diceResults[1]})`,
+          value: `![Dice ${diceResults[1]}](${FINAL_DICE_FACES[diceResults[1]]}) (${diceResults[1]})`,
           inline: true,
         },
         {
           name: '🎯 Xúc xắc 3',
-          value: `![Dice ${diceResults[2]}](${DICE_FACE_GIFS[diceResults[2]]}) (${diceResults[2]})`,
+          value: `![Dice ${diceResults[2]}](${FINAL_DICE_FACES[diceResults[2]]}) (${diceResults[2]})`,
           inline: true,
         },
       ],
